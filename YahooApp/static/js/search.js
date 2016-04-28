72cbaad5005208f45c7f461ba5df78de09afbb9e
Kevin Hung <kevinhung@C02K4L.local>
2016-04-28 18:27:20 -0500

    scroll section animation kinda working

--- YahooApp/static/js/search.js
+++ YahooApp/static/js/search.js
@@ -1,8 +1,23 @@
 
-$(window).scroll(function(){
-  var $searchbar = $('.srch-bar').height();
-  if($(this).scrollTop() == $searchbar){
-    window.scrollTo(0, 500);
-    return false;
-    };
+$(window).scroll(function(e){
+  var count=0;
+  if(window.pageYOffset>=100 && count==0){
+    // Prevent a page reload when a link is pressed
+    e.preventDefault();
+    // Call the scroll function
+    $('html,body').animate({
+        scrollTop: $(".newsframe1").offset().top},
+        'slow');
+        count+=1;
+        console.log(count);
+        console.log(pageYOffset);
+  } else if(window.pageYOffset>=950 && count==1){
+    // Prevent a page reload when a link is pressed
+    e.preventDefault();
+    // Call the scroll function
+    $('html,body').animate({
+        scrollTop: $(".logo").offset().top},
+        'slow');
+        count-=1;
+  }
 });
