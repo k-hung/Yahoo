fa2e400ab5bf167806702ba48e4aac0fc37c677c
Kevin Hung <kevinhung@C02K4L.local>
2016-04-28 20:02:55 -0500

    last changes before presentation, commented out the scroll js

--- YahooApp/static/js/search.js
+++ YahooApp/static/js/search.js
@@ -1,23 +1,14 @@
-
-$(window).scroll(function(e){
-  var count=0;
-  if(window.pageYOffset>=100 && count==0){
-    // Prevent a page reload when a link is pressed
-    e.preventDefault();
-    // Call the scroll function
-    $('html,body').animate({
-        scrollTop: $(".newsframe1").offset().top},
-        'slow');
-        count+=1;
-        console.log(count);
-        console.log(pageYOffset);
-  } else if(window.pageYOffset>=950 && count==1){
-    // Prevent a page reload when a link is pressed
-    e.preventDefault();
-    // Call the scroll function
-    $('html,body').animate({
-        scrollTop: $(".logo").offset().top},
-        'slow');
-        count-=1;
-  }
-});
+//
+// $(window).scroll(function(e){
+//   if(window.pageYOffset>=100){
+//     // Prevent a page reload when a link is pressed
+//     e.preventDefault();
+//     // Call the scroll function
+//     $('html,body').animate({
+//         scrollTop: $(".newsframe1").offset().top},
+//         'slow');
+//         count+=1;
+//         console.log(count);
+//         console.log(pageYOffset);
+//       }
+// });
